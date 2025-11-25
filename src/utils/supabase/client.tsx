3920b0d7a4460937bdcd7b9ca69239e035025d66
devarshdev001-creator <devarsh.dev001@gmail.com>
2025-11-25 13:18:25 +0530

    Add files via upload

---
+++ src/utils/supabase/client.tsx
@@ -0,0 +1,12 @@
+import { createClient as createSupabaseClient } from '@supabase/supabase-js@2';
+import { projectId, publicAnonKey } from './info.tsx';
+
+let supabaseInstance: ReturnType<typeof createSupabaseClient> | null = null;
+
+export function createClient() {
+  if (!supabaseInstance) {
+    const supabaseUrl = `https://${projectId}.supabase.co`;
+    supabaseInstance = createSupabaseClient(supabaseUrl, publicAnonKey);
+  }
+  return supabaseInstance;
+}
